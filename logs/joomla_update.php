766ed60b8a784b77e91326163781fd1e25de58c1
bobozhangshao <1310255342@qq.com>
2016-05-10 17:18:32 +0800

    版本升级3.4.8->3.5.1

---
+++ logs/joomla_update.php
@@ -0,0 +1,40 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2016-05-10 06:53:21 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2016-05-10T06:53:21+00:00	INFO 127.0.0.1	update	用户Super User（726）开始升级工作。旧版本为 3.4.8 。
+2016-05-10T06:53:23+00:00	INFO 127.0.0.1	update	从 https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.1-Stable-Update_Package.zip 下载升级文件
+2016-05-10T06:54:05+00:00	INFO 127.0.0.1	update	用户Super User（726）开始升级工作。旧版本为 3.4.8 。
+2016-05-10T06:54:06+00:00	INFO 127.0.0.1	update	从 https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.1-Stable-Update_Package.zip 下载升级文件
+2016-05-10T06:56:05+00:00	INFO 127.0.0.1	update	用户Super User（726）开始升级工作。旧版本为 3.4.8 。
+2016-05-10T06:56:06+00:00	INFO 127.0.0.1	update	从 https://github.com/joomla/joomla-cms/releases/download/3.5.1/Joomla_3.5.1-Stable-Update_Package.zip 下载升级文件
+2016-05-10T06:57:32+00:00	INFO 127.0.0.1	update	已成功下载文件 Joomla_3.5.1-Stable-Update_Package.zip 。
+2016-05-10T07:00:31+00:00	INFO 127.0.0.1	update	已成功下载文件 Joomla_3.5.1-Stable-Update_Package.zip 。
+2016-05-10T08:55:45+00:00	INFO 127.0.0.1	update	用户Super User（726）开始升级工作。旧版本为 3.4.8 。
+2016-05-10T08:55:48+00:00	INFO 127.0.0.1	update	已成功下载文件 Joomla_3.5.1-Stable-Update_Package.zip 。
+2016-05-10T08:55:48+00:00	INFO 127.0.0.1	update	开始安装新版本。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	正在完成安装
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-07-01。查询文本：ALTER TABLE `#__session` MODIFY `session_id` varchar(191) NOT NULL DEFAULT '';。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-07-01。查询文本：ALTER TABLE `#__user_keys` MODIFY `series` varchar(191) NOT NULL;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-10-13。查询文本：INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-10-26。查询文本：ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_tag`;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-10-26。查询文本：ALTER TABLE `#__contentitem_tag_map` DROP INDEX `idx_type`;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-10-30。查询文本：UPDATE `#__menu` SET `title` = 'com_contact_contacts' WHERE `id` = 8;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-11-04。查询文本：DELETE FROM `#__menu` WHERE `title` = 'com_messages_read' AND `client_id` = 1;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-11-04。查询文本：INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-11-05。查询文本：INSERT INTO `#__extensions` (`extension_id`, `name`, `type`, `element`, `folder`。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2015-11-05。查询文本：INSERT INTO `#__postinstall_messages` (`extension_id`, `title_key`, `description。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-02-26。查询文本：CREATE TABLE IF NOT EXISTS `#__utf8_conversion` (   `converted` tinyint(4) NOT N。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-02-26。查询文本：INSERT INTO `#__utf8_conversion` (`converted`) VALUES (0);。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-03-01。查询文本：ALTER TABLE `#__redirect_links` DROP INDEX `idx_link_old`;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-03-01。查询文本：ALTER TABLE `#__redirect_links` MODIFY `old_url` VARCHAR(2048) NOT NULL;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-03-01。查询文本：ALTER TABLE `#__redirect_links` MODIFY `new_url` VARCHAR(2048) NOT NULL;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-03-01。查询文本：ALTER TABLE `#__redirect_links` MODIFY `referer` VARCHAR(2048) NOT NULL;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.0-2016-03-01。查询文本：ALTER TABLE `#__redirect_links` ADD INDEX `idx_old_url` (`old_url`(100));。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.1-2016-03-25。查询文本：ALTER TABLE `#__user_keys` MODIFY `user_id` varchar(150) NOT NULL;。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	从该文件运行查询：3.5.1-2016-03-29。查询文本：UPDATE `#__utf8_conversion` SET `converted` = 0  WHERE (SELECT COUNT(*) FROM `#_。
+2016-05-10T08:55:52+00:00	INFO 127.0.0.1	update	删除已没用的文件和目录。
+2016-05-10T08:55:56+00:00	INFO 127.0.0.1	update	安装后清理现场
+2016-05-10T08:55:56+00:00	INFO 127.0.0.1	update	已完成到版本 3.5.1 的升级。
